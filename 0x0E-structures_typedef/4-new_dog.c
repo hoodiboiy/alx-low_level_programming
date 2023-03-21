8a0719f096f1bbaf09df637dc6ae57e0850dbb20
hoodiboiy <ossaihandsome@gmail.com>
2023-03-20 19:55:27 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -78,14 +78,14 @@ int _strlen(char *s)
   */
 char *_strcpy(char *dest, char *src)
 {
-	int
-I;
+	int i;
 
 	for (i = 0; src[i] != '\0'; i++)
 	{
-		dest[i] = src[i]
+		dest[i] = src[i];
+	}
+
 	dest[i++] = '\0';
 
 	return (dest);
 }
-	}
